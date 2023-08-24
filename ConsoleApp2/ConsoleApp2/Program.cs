8d7d18168afba38ff119661de8037fd2e93f6a56
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-24 20:59:09 -0300

    Subindo arquivos para o git

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+while (true){
+    DateTime data = DateTime.Now;
+    int hora = data.Hour;
+    int minuto = data.Minute;
+    int seg = data.Second;
+    Console.Write("\r{0:D2}:{1:D2}:{2:D2}",hora,minuto,seg);
+    Thread.Sleep(1000);
+    if (Console.KeyAvailable)
+    {
+        ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true);
+        if( keyInfo.Key == ConsoleKey.X)
+        {
+            break;
+        }
+    }
+}
